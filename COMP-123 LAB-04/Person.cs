fea9ac1b5f74523e07c766ad5aeb0befe7477b2d
Sukhjinder Kaur <sukhmanyu@gmail.com>
2020-06-17 15:16:02 -0400

    Add project Files

---
+++ COMP-123 LAB-04/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_LAB_04
+{
+    class Person
+    {//PRIVATE INSTANCE VARIABLE
+        private string m_name;
+        private UInt16 m_age;
+
+        //PUBLIC PROPERTIES
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+            set
+            {
+                m_name = value;
+            }
+        }
+        public UInt16 Age
+        {
+            get
+            {
+                return m_age;
+            }
+            set
+            {
+                m_age = value;
+            }
+        }
+        //CONSTRUCTORS
+        Person(string name, UInt16 age)
+        {
+            m_name = name;
+            m_age = age;
+        }
+        //PRIVATE METHODS
+        //PUBLIC METHODS
+        public void SayHello()
+        {
+            Console.WriteLine($"{Name} says hello");
+        }
+    }
+}
---
+++ COMP-123 LAB-04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP_123_LAB_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person sukh = new Person("Sukhjinder Kaur", 30);
+        }
+    }
+}
